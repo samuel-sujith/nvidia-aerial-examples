e24a8b083e29f2a088df1d0043adeb8ef1c83b19
Sujith Samuel <Sujith.Samuel@nokia.com>
2026-01-02 07:27:42 +0000

    Correcting code

---
+++ quill/LogMacros.h
@@ -0,0 +1,25 @@
+/*
+ * Minimal Quill LogMacros.h stub for compilation compatibility
+ */
+#pragma once
+
+// No-op logging macros
+#define QUILL_LOG_TRACE_L1(...) do { } while(0)
+#define QUILL_LOG_TRACE_L2(...) do { } while(0) 
+#define QUILL_LOG_TRACE_L3(...) do { } while(0)
+#define QUILL_LOG_DEBUG(...)    do { } while(0)
+#define QUILL_LOG_INFO(...)     do { } while(0)
+#define QUILL_LOG_WARNING(...)  do { } while(0)
+#define QUILL_LOG_ERROR(...)    do { } while(0)
+#define QUILL_LOG_CRITICAL(...) do { } while(0)
+
+// Additional macros that might be used
+#define QUILL_LOG(...) do { } while(0)
+#define LOG_TRACE_L1(...) do { } while(0)
+#define LOG_TRACE_L2(...) do { } while(0) 
+#define LOG_TRACE_L3(...) do { } while(0)
+#define LOG_DEBUG(...)    do { } while(0)
+#define LOG_INFO(...)     do { } while(0)
+#define LOG_WARNING(...)  do { } while(0)
+#define LOG_ERROR(...)    do { } while(0)
+#define LOG_CRITICAL(...) do { } while(0)
